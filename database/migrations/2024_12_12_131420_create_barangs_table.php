740ea10f581e4e6154158d98285e9850c248c700
kentangkotak <ariezlnd69@gmail.com>
2024-12-12 20:33:43 +0700

    wip : buat tabel dan model

---
+++ database/migrations/2024_12_12_131420_create_barangs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kodebarang');
+            $table->string('namabarang');
+            $table->string('satuan_b');
+            $table->string('satuan_k');
+            $table->decimal('isi');
+            $table->string('kategori');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
