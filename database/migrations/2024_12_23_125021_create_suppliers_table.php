a7da9d88ec121b40272582a4a71059829b467e0c
kentangkotak <68070888+kentangkotak@users.noreply.github.com>
2024-12-23 19:56:49 +0700

    wew

--- database/migrations/2024_12_23_125021_create_suppliers_table.php
+++ database/migrations/2024_12_23_125021_create_suppliers_table.php
@@ -11,17 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('nama')->nullable();
-            $table->string('username')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->string('jabatan')->nullable();
-            $table->string('nohp')->nullable();
             $table->string('alamat')->nullable();
-            $table->rememberToken();
+            $table->string('telepon')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('suppliers');
     }
 };
